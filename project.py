a8e0c540141a387d9bb9950e4f8d940f6858d28c
Jal-Samuel-tech <jalsamuel089@gmail.com>
2025-12-18 13:57:24 +0300

    commit name

--- project.py
+++ project.py
@@ -7,3 +7,9 @@ if age>=18:
     print("You are eligible to vote.")
 else:
     print("You are not eligible to vote.")
+
+name=input("Enter your name: ")
+if name:           
+    print("Hello, " + name + "!")   
+else:
+    print("You didn't enter a name.")
