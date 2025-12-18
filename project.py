bc5a1f5d3e8d68a6d5d3adf6197c55144e4890b4
Jal-Samuel-tech <jalsamuel089@gmail.com>
2025-12-18 13:48:04 +0300

    Age criteria

--- project.py
+++ project.py
@@ -1,3 +1,9 @@
 name="Ajal"
 for letter in name:
     print(letter)   
+
+age=float(input("Enter your age: "))
+if age>=18:         
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
