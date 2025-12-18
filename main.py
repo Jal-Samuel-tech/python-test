939f4e8fe1bb6f5aeca639805c2da7ee8cbc69ee
Jal-Samuel-tech <jalsamuel089@gmail.com>
2025-12-18 14:06:40 +0300

    if statement

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ my_list.append(6)
 print(my_list)  # Output: [1, 2, 3, 4, 5, 6]\
 my_list.sort(reverse=True)
 print(my_list)  # Output: [1, 3, 4, 5, 6]
+my_list.remove(3)
+print(my_list)  # Output: [6, 5, 4, 1]
+my_list.insert(2, 10)
+print(my_list)  # Output: [6, 5, 10, 4, 1]
+print("Hello, World!")
+for i in range(1, 6):
+    print(i)
+for i in range(5, 0, -1):
+    print(i)
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+for i in range(1, 11):
+    if i % 2 != 0:
+        print(i)
